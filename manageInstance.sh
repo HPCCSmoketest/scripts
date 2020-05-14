ac85a15e3e79bf31577296ad29daff3cfe12df95
HPCCSmoketest <hpccsmoketest@gmail.com>
2020-05-15 00:42:43 +0100

    Deploy on 2020-05-05 00:42:43
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- manageInstance.sh
+++ manageInstance.sh
@@ -19,7 +19,7 @@ res=$( declare -f -F WriteLog  2>&1 )
     
 if [ $? -ne 0 ]
 then
-    echo "WriteLog() function is missing (${res}) try to import again"
+    echo "WriteLog() function is missing (${res}, cwd: $(pwd)) try to import again"
     . ~/smoketest/timestampLogger.sh
 else
     WriteLog "res: ${res}" "$LOG_FILE"
