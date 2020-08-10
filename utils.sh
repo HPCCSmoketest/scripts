e5612e37aabe1082ebe6ea3499b87891bb986c3e
HPCCSmoketest <hpccsmoketest@gmail.com>
2020-08-10 12:35:28 +0100

    Deploy on 2020-08-08 12:35:28
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- utils.sh
+++ utils.sh
@@ -61,7 +61,7 @@ TARGET=all
 
 GLOBAL_EXCLUSION="-e 3rdparty"
 GLOBAL_EXCLUSION="-e=embedded,3rdparty"
-GLOBAL_EXCLUSION="--ef pipefail.ecl -e=embedded,3rdparty -r=python3"
+GLOBAL_EXCLUSION="--ef pipefail.ecl -e=embedded,3rdparty"
 PYTHON_PLUGIN=''
 
 #
