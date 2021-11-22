b641fba5c256902f581df5ab4f4b0b5d9a7042a5
HPCCSmoketest <hpccsmoketest@gmail.com>
2021-11-22 19:37:03 +0000

    Deploy the latest version. Added 'Base branch' and 'Jira' columns. Fixed Commit ID handling issue. Removed the large, commented out code block.
    Deploy on 2021-11-22 19:37:02
    
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- listTests.py
+++ listTests.py
@@ -57,6 +57,8 @@ def update():
     starts = []
     ends = []
     ellapses = []
+    bases = []
+    jiras = []
 
     if len(files) > 0:
         index = 0
@@ -81,24 +83,30 @@ def update():
                 jira = 'N/A'
 
                 for item in items:
+                    item = item.strip()
+
+                    itemIsHexString = False
+                    # Check whether it is a hexadecmal string (commit id)
+		    try:
+                        i = int(item, 16)
+                        itemIsHexString = True
+                    except ValueError:
+                        pass
+
                     if item.startswith('PR-') and pr == 'N/A':
-                        pr = item.strip()
+                        pr = item
                     elif item.startswith('i-'):
-                        instance = item.strip()
-                    elif item.startswith('HPCC-'):
-                        jira = item.strip()
+                        instance = item
+                    elif item.startswith('HPCC-') or item.startswith('PR-wo-'):
+                        jira = item
                     elif item.startswith('master') or item.startswith('candidate'):
-			base = item.strip()
-                    else:
-                        commit = item.strip()
-                        # Check whether it is a hexadecmal string (commit id)
-			try:
-                      	    i = int(commit, 16)
-                        except ValueError:
-                            commit = 'N/A'
+			base = item
+                    elif itemIsHexString:
+                        commit = item
 			if len(commit) > 0 and 'N/A' != commit:
                             status = 'running'
-
+                    else:
+                        print("Unknown item: '%s'" % (item))
                 
 		if 'OldPr' in f:
                     status = 'closed'
@@ -146,7 +154,8 @@ def update():
                 e = int(startTimestamp - tests[pr][instance]['startTimestamp'])
                 #ellapses.append("%d sec" % ( e ))
                 ellapses.append("%d sec (%s)" % (e, time.strftime("%H:%M:%S", time.gmtime(e))))
-        
+                bases.append(base)
+                jiras.append(jira)        
 	
 	# get the results
         myProcRes = subprocess.Popen(["find OldPrs/PR-*/ PR-*/ -iname 'result-'" + testDay + "'*.log' -type f  -print | sort "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
@@ -251,147 +260,6 @@ def update():
 
         print(tests)
 
-        '''
-        lastMsgIndex = -1
-        while len(result[lastMsgIndex]) == 0:
-           lastMsgIndex -= 1
-
-        lastMsg = result[lastMsgIndex]
-        print("\tlastMsg: %s \n\tindex of lastMsg: %d" % (lastMsg,  lastMsgIndex))
-
-        if lastMsg.startswith('Wait') or lastMsg.startswith('start'): 
-            divCurrentState.text = 'Idle'
-        elif lastMsg.startswith('Done, exit'):
-            divCurrentState.text = 'Stopped'
-        elif lastMsg.startswith('--- PR'):
-            divCurrentState.text = "Busy"
-
-        index = lastMsgIndex 
-        pr = "none"
-        user = "none"
-        startTimeStr = ""
-        phase = "N/A"
-        subPhase = ""
-        ect = "N/A"
-        try:
-            while (index > -10):
-                line = result[index].strip()
-                if line.startswith('end'):
-                    break
-                    
-                if line.lower().startswith('user'):
-                    user = line.split(':')[1]
-
-                if line.lower().startswith('start:'):
-                    startTimeStr = line.split(':', 1)[1].strip()
-                    startTime = datetime.strptime(startTimeStr, "%y-%m-%d  %H:%M:%S" )
-                                  
-                m = re.match('^([0-9]*)/([0-9]*)\. Process of (PR-[0-9]*)(.*)', line)
-                if m:
-                    pr = m.group(3)
-                    # Check the PR directory and process build log file is it is exists
-                    if  os.path.exists(pr):
-                        # if we have multiple build/test file use the latest.
-                        logfiles = sorted(glob.glob(pr + '/*_Build_*.log'), reverse = True)
-                        if len(logfiles) > 0:
-                            # Check the logfile is relevant or not
-                            logFileTimeStr = logfiles[0].split('_', 2)[2].replace('.log', '').strip()
-                            logFileTime = datetime.strptime(logFileTimeStr, "%Y-%m-%d_%H-%M-%S" )
-                            if logFileTime >= startTime:
-                                print("PR logfile:%s, " % (logfiles[0])),
-
-                                myProc2 = subprocess.Popen(["cat "+ logfiles[0] + " | egrep -i 'milestone' | tail -n 1 "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-                                result2 = myProc2.stdout.read() + myProc2.stderr.read()
-                                if len(result2) > 0:
-                                    items = result2.split(':', 1)
-                                    if len(items) == 2:
-                                        phase = items[1]
-                                else:
-                                    phase = "Not registered"
-                                
-                                #print("phase: %s" % (phase))
-                                
-                                if ('nstall' in phase) or ('uild' in phase):
-                                    myProc3 = subprocess.Popen(["  egrep -i '\[[0-9 ]*\%\]' " + logfiles[0] + " | tail -n 1 "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-                                    result3 = myProc3.stdout.read() + myProc3.stderr.read()
-                                    if len(result3) > 0:
-                                        result3Items = result3.replace('[', '').replace(']','') .split(' ',  2)
-                                        
-                                        if len(result3Items) >= 2:
-                                                subPhase += result3Items[1]+ " "
-                                    
-                                elif 'egression' in phase:
-                                    myProc3 = subprocess.Popen([" egrep -i -A1 'Suite:' " + logfiles[0] + " | tail -n 2 "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-                                    result3 = myProc3.stdout.read() + myProc3.stderr.read()
-                                    if len(result3) > 0:
-                                        prefix = ["E: ", ", T: "]
-                                        index = 0
-                                        result3Items = result3.split('\n')
-                                        for item in result3Items:
-                                            items = item.split(':', 1)
-                                            if len(items) == 2:
-                                                subPhase += prefix[index] + items[1].strip()
-                                                index += 1
-                                        
-                                        myProc4 = subprocess.Popen([" egrep -i  -i '\[Action\]' " + logfiles[0] + " | tail -n 1 "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-                                        result4 = myProc4.stdout.read() + myProc4.stderr.read()
-                                        if len(result4) > 0:
-                                            result4Items = result4.split(' ',  4)
-                                            if len(result4Items) >= 2:
-                                                # There are leading spaces before the test number and the split() generates empty items from them
-                                                # Should look for the first non empty
-                                                index = 1
-                                                while (result4Items[index] == ''):
-                                                    index += 1
-                                                subPhase += ", R: " + result4Items[index].strip('.') + " "
-                                                
-                                            else:
-                                                subPhase += "0 "
-                                                
-                                        myProc5 = subprocess.Popen([" suite=$( egrep -i 'suite:' " + logfiles[0] + " | tail -n 1 ); suite=${suite//\[Action\]/};  section=$( sed -rn \"/$suite$/,/$suite$/p\" " + logfiles[0] + " ); echo \"$section\" | egrep -i -c ' Pass ';  echo \"$section\" | egrep -i -c ' Fail '; "],  shell=True,  bufsize=8192,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-                                        result5 = myProc5.stdout.read() + myProc5.stderr.read()
-                                        if len(result5) > 0:
-                                            prefix = [", P: ", ", F: "]
-                                            result5Items = result5.split()
-                                            if len(result5Items) >= 1:
-                                                for index in range(len(result5Items)):
-                                                    subPhase += prefix[index] + result5Items[index].strip('.')
-                                                
-                                            else:
-                                                subPhase += "/ - /-  "
-                                        
-                                if len(subPhase) > 0:
-                                    subPhase = ' ( ' + subPhase + ') '
-                                        
-                    m2 = re.match('.* ~(.*) hour.*$', m.group(4))
-                    if m2:
-                        _ect = float(m2.group(1))
-                        
-                        ectTime = startTime + timedelta(hours = _ect)
-                        now =  datetime.now()
-                        isInTime = ''
-                        isOver = ''
-                        if ectTime >= now:
-                            remainTime = (ectTime.hour * 3600 + ectTime.minute * 60 + ectTime.second) - (now.hour * 3600 + now.minute * 60 + now.second)
-                            isInTime = '-'
-                        else:
-                            remainTime = (now.hour * 3600 + now.minute * 60 + now.second) - (ectTime.hour * 3600 + ectTime.minute * 60 + ectTime.second) 
-                            isOver = 'overrun'
-                            print ("%s is overrun with %s seconds" % (pr, remainTime))
-
-                        remainTimeStr = "%s%2dh %2dm %s" % (isInTime, remainTime / 3600, (remainTime % 3600) / 60,  isOver)
-                        ect = "~ %s (%s)" % (ectTime.strftime("%H:%M"), remainTimeStr )
-                        break
-                index -= 1
-        except:
-            print("\tIndex overflow: %d/%d." % (index, len(result))), 
-            pass
-        
-        divCurrentPr.text = pr 
-        divCurrentUser.text = user
-        divCurrentEct.text = ect
-        divCurrentPhase.text = phase + subPhase
-        '''
     else:
         print("\tlen prs: %d" % (len(prs)))
         if len(prs) == 0:
@@ -412,6 +280,8 @@ def update():
     source.data = {
         'pr' : prs,
         'instance' : instances,
+        'base' : bases,
+        'jira' : jiras,
         'scheduledCommit' : scheduledCommits,
         'testedCommit' : testedCommits,
         'status' : statuses,
@@ -434,6 +304,8 @@ def update():
 columns = [
     TableColumn(field='pr', title= 'PR', formatter=StringFormatter(text_align='center',  text_color='#000000')),
     TableColumn(field='instance', title= 'Instance', formatter=StringFormatter(text_align='center',  text_color='#000000')),
+    TableColumn(field='base', title= 'Base branch', formatter=StringFormatter(text_align='center',  text_color='#000000')),
+    TableColumn(field='jira', title= 'Jira', formatter=StringFormatter(text_align='center',  text_color='#000000')),
     TableColumn(field='scheduledCommit', title= 'Scheduled commit', formatter=StringFormatter(text_align='center',  text_color='#000000')),
     TableColumn(field='testedCommit', title= 'Tested commit', formatter=StringFormatter(text_align='center',  text_color='#000000')),
     TableColumn(field='start', title= 'Start', formatter=StringFormatter(text_align='center',  text_color='#000000')),
@@ -443,7 +315,7 @@ columns = [
     TableColumn(field='result', title= 'Result', formatter=StringFormatter(text_align='center',  text_color='#000000')),
     ]
     
-dataTable = DataTable(source=source,  columns=columns,  width=1100)
+dataTable = DataTable(source=source,  columns=columns,  width=1300 )
 
 # If no row(s) selected and dataTable 'data' changed then this scrolls down to the last lines.
 data_table_force_change = CustomJS(args=dict(source=dataTable),  code="""
