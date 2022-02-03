ed0879516aba19600515498e7d2c2dd049c3ba4d
HPCCSmoketest <hpccsmoketest@gmail.com>
2022-02-03 16:59:23 +0000

    Fix Bokeh default URL (now the IP  points to 15.222.244.18 - Smoketest in AWS as well).
    Deploy on 2022-02-03 16:59:23
    
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- startBokeh_templ.sh
+++ startBokeh_templ.sh
@@ -27,7 +27,7 @@ fi
 # The --allow-websocket-origin parameter(s) should update accordingly the real environment.
 unbuffer ${BOKEH} serve ${PYTHON_APPS} \
    --allow-websocket-origin=ec2-15-222-244-18.ca-central-1.compute.amazonaws.com:5006 \
-   --allow-websocket-origin=10.224.20.54:5006
+   --allow-websocket-origin=15.222.244.18:5006
 
 # On ONT-011 we can use hostname
 #unbuffer bokeh serve ${PYTHON_APPS} --allow-websocket-origin=$(hostname):5006
