3843ac595c3189ded91584a4f2515abfd60c36ba
HPCCSmoketest <hpccsmoketest@gmail.com>
2022-09-02 21:41:59 +0100

    Exclude spray tests was in the wrong place. Fixed.
    Deploy on 2022-09-02 21:41:59
    
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- build.sh
+++ build.sh
@@ -334,8 +334,6 @@ done
 LOGLEVEL=info
 WritePlainLog "Loglevel: ${LOGLEVEL}" "$logFile"
 
-GLOBAL_EXCLUSION="-e spray"
-
 #--------------------------------------
 # Check plugins (especially Pythons) and apply exclusion if it is need
 
