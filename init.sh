36a4c5ab69dd83809a1db5422fb734d7db3ee247
HPCCSmoketest <hpccsmoketest@gmail.com>
2024-09-20 19:42:51 +0100

    init.sh: - Comment out 'sudo yum clean all' and 'sudo yum makecache' because the   outdated CentOS 7 they are prevent to install 'mono-complete'
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- init.sh
+++ init.sh
@@ -385,11 +385,12 @@ then
     res=$(sudo yum-config-manager --add-repo http://download.mono-project.com/repo/centos/ 2>&1)
     myEcho "Res: $res"
     
-    res=$(sudo yum clean all 2>&1)
-    myEcho "Res: $res"
+    # Based on CentOS 7 is out of scope these two commands prevent to install mono-complete
+    #res=$(sudo yum clean all 2>&1)
+    #myEcho "Res: $res"
     
-    res=$(sudo yum makecache 2>&1)
-    myEcho "Res: $res"
+    #res=$(sudo yum makecache 2>&1)
+    #myEcho "Res: $res"
         
     res=$(sudo rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF" 2>&1)
     myEcho "Res: $res"
