324e58a2431f4f58be0931f952d46d9600f6c66d
HPCCSmoketest <hpccsmoketest@gmail.com>
2025-01-07 17:50:47 +0000

    init-rocky8.sh: - Add commands to use nvm without resopen the console
    Signed-off-by: HPCCSmoketest <hpccsmoketest@gmail.com>

--- init-rocky8.sh
+++ init-rocky8.sh
@@ -125,6 +125,9 @@ sudo yum --enablerepo=nodesource clean metadata
 
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
 # Download and install Node.js:
 nvm install 22
